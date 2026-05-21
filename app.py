d11dd7889edc3d162855857e5a230b1c469378e2
Art Behluli <art.behluli@git.com>
2026-05-21 11:25:08 +0200

    Add /info route

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ def hello():
     name = request.args.get('name', 'World')
     return f'Hello, {name}!'
 
+@app.route('/info')
+def info():
+    return {
+        'app': 'Flask Python Server',
+        'version': '1.0',
+        'author': 'Art Behluli'
+    }
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+
